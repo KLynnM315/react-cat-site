435564a1ec19e81c8ecec588fe3a5e24e2c1478d
Kristy Codes <KLynnM333@gmail.com>
2021-11-17 21:11:19 -0500

    Added basic class to file

---
+++ src/components/ShoppingForCats/ShoppingForCats.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ShoppingForCats extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Shopping List for FUN</h1>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  export default ShoppingForCats;
