7ea7417c16e7c302b5b5b318d154a4d8d50bf86c
Kristy Codes <KLynnM333@gmail.com>
2021-11-17 21:10:12 -0500

    removed the route for now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import SideBar from './components/SideBar/SideBar';
+import SideBar from './components/SideBar';
 
-import ShoppingForCats from "./components/ShoppingForCats/ShoppingForCats"
+import ShoppingForCats from "./components/ShoppingForCats"
 
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+        <ShoppingForCats />
 
-      <BrowserRouter  > 
-        <SideBar />
-
-        <Route>
-          <Link to="/catfax" component={ShoppingForCats} />
-          <Link to="/cattxt" component={ShoppingForCats} />
-        </Route>
-
-      </BrowserRouter>
 
     </div>
   );
+
+
 }
 
 export default App;
